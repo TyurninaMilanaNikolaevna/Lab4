2d801dc3c6ba32c296c4c3a3f46cbc54cd2c246a
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-10 20:27:49 +0300

    Scripted auto-commit on change (2021-12-10 20:27:49) by gitwatch.sh

--- src/main/java/lab4/TestingApp.java
+++ src/main/java/lab4/TestingApp.java
@@ -35,7 +35,7 @@ public class TestingApp {
         final Flow<HttpResponse, HttpRequest, NotUsed>
                 flow = instance.createRoute().flow(system, actorMaterializer);
 
-        final CompletionStage<ServerBinding> 
+        final CompletionStage<ServerBinding> bindingCompletionStage = 
 
     }
 

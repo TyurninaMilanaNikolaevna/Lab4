e4cf471c67ebd2088628b286ab2f673e2e11a071
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-12 19:49:28 +0300

    Scripted auto-commit on change (2021-12-12 19:49:28) by gitwatch.sh

--- src/main/java/lab4/StoringResultActor.java
+++ src/main/java/lab4/StoringResultActor.java
@@ -1,10 +1,11 @@
 package lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class StoringResultActor extends AbstractActor{
     @Override
     public Receive createReceive() {
-        
+        return ReceiveBuilder
     }
 }

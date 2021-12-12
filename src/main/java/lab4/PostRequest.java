0ac96089099bf9249dc8be29aefcc4663b9b9767
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-12 19:10:50 +0300

    Scripted auto-commit on change (2021-12-12 19:10:49) by gitwatch.sh

--- src/main/java/lab4/PostRequest.java
+++ src/main/java/lab4/PostRequest.java
@@ -1,12 +1,15 @@
 package lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 
 public class PostRequest {
 
     ArrayList<Testing> testings;
 
-    public PostRequest() {
-        
+    public PostRequest(@JsonProperty("packageId") String packageId,
+                       @JsonProperty()) {
+
     }
 }

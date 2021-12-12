6f44a78d8584ee2b77bf54e90cba9940c6c02deb
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-12 18:55:49 +0300

    Scripted auto-commit on change (2021-12-12 18:55:49) by gitwatch.sh

--- src/main/java/lab4/GetRequest.java
+++ src/main/java/lab4/GetRequest.java
@@ -1,7 +1,11 @@
 package lab4;
 
 public class GetRequest {
-
     private final String packageId;
 
+    public GetRequest(String packageId) {
+        this.packageId = packageId;
+    }
+
+    
 }

48f29eec74ad2b3a278909c3c9806a255fae8fb8
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-16 18:37:11 +0300

    Scripted auto-commit on change (2021-12-16 18:37:11) by gitwatch.sh

--- src/main/java/lab4/PerformingTestActor.java
+++ src/main/java/lab4/PerformingTestActor.java
@@ -3,6 +3,9 @@ package lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.*;
+import java.io.Reader;
+
 public class PerformingTestActor {
 
     public class StoringResultActor extends AbstractActor {
@@ -14,4 +17,75 @@ public class PerformingTestActor {
                         sender().tell(testing, self())
                     })
         }
+
+        public String perform(Testing testing) {
+            ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("")
+
+                @Override
+                public Object eval(Reader reader, ScriptContext context) throws ScriptException {
+                    return null;
+                }
+
+                @Override
+                public Object eval(String script) throws ScriptException {
+                    return null;
+                }
+
+                @Override
+                public Object eval(Reader reader) throws ScriptException {
+                    return null;
+                }
+
+                @Override
+                public Object eval(String script, Bindings n) throws ScriptException {
+                    return null;
+                }
+
+                @Override
+                public Object eval(Reader reader, Bindings n) throws ScriptException {
+                    return null;
+                }
+
+                @Override
+                public void put(String key, Object value) {
+
+                }
+
+                @Override
+                public Object get(String key) {
+                    return null;
+                }
+
+                @Override
+                public Bindings getBindings(int scope) {
+                    return null;
+                }
+
+                @Override
+                public void setBindings(Bindings bindings, int scope) {
+
+                }
+
+                @Override
+                public Bindings createBindings() {
+                    return null;
+                }
+
+                @Override
+                public ScriptContext getContext() {
+                    return null;
+                }
+
+                @Override
+                public void setContext(ScriptContext context) {
+
+                }
+
+                @Override
+                public ScriptEngineFactory getFactory() {
+                    return null;
+                }
+            }
+        }
+    }
 }

1172d4b0ab8313e0b6333afd1d9ec5b74428b562
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-16 17:40:55 +0300

    Scripted auto-commit on change (2021-12-16 17:40:55) by gitwatch.sh

--- src/main/java/lab4/RouterActor.java
+++ src/main/java/lab4/RouterActor.java
@@ -1,13 +1,14 @@
 package lab4;
 
 import akka.actor.ActorRef;
+import akka.actor.Props;
 
 public class RouterActor {
 
     private final int MAX_RETRIES = 10;
 
     public RouterActor() {
-        storingResultActor = getContext().actorOf
+        storingResultActor = getContext().actorOf(Props.create())
 
     }
 }

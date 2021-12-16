d619dbd645ebb3b32ee89b5fd85e4a4426b57bd2
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-16 18:33:16 +0300

    Scripted auto-commit on change (2021-12-16 18:33:16) by gitwatch.sh

--- src/main/java/lab4/PostRequest.java
+++ src/main/java/lab4/PostRequest.java
@@ -7,21 +7,17 @@ import java.util.ArrayList;
 public class PostRequest {
 
     ArrayList<Testing> testings;
-    private  int packageId;
-    private String jsScript;
-    private String functionName;
 
     public PostRequest(@JsonProperty("packageId") String packageId,
                        @JsonProperty("jsScript") String jsScript,
                        @JsonProperty("functionName") String functionName,
                        @JsonProperty("testings") ArrayList<Testing> testings) {
-        this.packageId.setPackageId(packageId);
-        this.jsScript.serJsScript(jsScript);
-        this.functionName = functionName;
         this.testings = testings;
 
-        for (int i = 0; i < testings.size(); i++) {
-            testings.set
+        for (Testing testing : this.testings) {
+            testing.setPackageId(packageId);
+            testing.setJsScript(jsScript);
+            testing.setFunctionName(functionName);
         }
     }
 

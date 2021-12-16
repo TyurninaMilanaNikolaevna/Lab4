07af2c4c785875fece6d832658868241c31896d2
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-16 18:40:26 +0300

    Scripted auto-commit on change (2021-12-16 18:40:26) by gitwatch.sh

--- src/main/java/lab4/PerformingTestActor.java
+++ src/main/java/lab4/PerformingTestActor.java
@@ -18,74 +18,13 @@ public class PerformingTestActor {
                     })
         }
 
-        public String perform(Testing testing) {
-            ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("nashorn")
+        public String performingTest(Testing testing) {
+            ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("nashorn");
 
-                @Override
-                public Object eval(Reader reader, ScriptContext context) throws ScriptException {
-                    return null;
-                }
+            
 
-                @Override
-                public Object eval(String script) throws ScriptException {
-                    return null;
-                }
 
-                @Override
-                public Object eval(Reader reader) throws ScriptException {
-                    return null;
-                }
 
-                @Override
-                public Object eval(String script, Bindings n) throws ScriptException {
-                    return null;
-                }
-
-                @Override
-                public Object eval(Reader reader, Bindings n) throws ScriptException {
-                    return null;
-                }
-
-                @Override
-                public void put(String key, Object value) {
-
-                }
-
-                @Override
-                public Object get(String key) {
-                    return null;
-                }
-
-                @Override
-                public Bindings getBindings(int scope) {
-                    return null;
-                }
-
-                @Override
-                public void setBindings(Bindings bindings, int scope) {
-
-                }
-
-                @Override
-                public Bindings createBindings() {
-                    return null;
-                }
-
-                @Override
-                public ScriptContext getContext() {
-                    return null;
-                }
-
-                @Override
-                public void setContext(ScriptContext context) {
-
-                }
-
-                @Override
-                public ScriptEngineFactory getFactory() {
-                    return null;
-                }
-            }
         }
     }
 }

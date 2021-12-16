fd478e3c81bfa090066d6aaeb59b960b63ae9f0e
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-16 18:50:36 +0300

    Scripted auto-commit on change (2021-12-16 18:50:36) by gitwatch.sh

--- src/main/java/lab4/PerformingTestActor.java
+++ src/main/java/lab4/PerformingTestActor.java
@@ -13,7 +13,7 @@ public class PerformingTestActor {
         public Receive createReceive() {
             return ReceiveBuilder.create()
                     .match(Testing.class, message -> {
-                        
+                        testing.s
                     })
         }
 

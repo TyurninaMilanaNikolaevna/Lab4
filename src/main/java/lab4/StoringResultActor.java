d5fd0197e5b0c1f07e4344751faffce1aed14887
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-16 17:19:12 +0300

    Scripted auto-commit on change (2021-12-16 17:19:12) by gitwatch.sh

--- src/main/java/lab4/StoringResultActor.java
+++ src/main/java/lab4/StoringResultActor.java
@@ -7,7 +7,7 @@ public class StoringResultActor {
     private final Map<String, Map<String, String>> storingResult = new HashMap<>();
 
     private void T (Testing testing) {
-        Map<String, String> packageResults =
+        Map<String, String> packageResults = storingResult.get();
     }
 
 
